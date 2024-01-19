ca89dd12939393486ea02dbef491420afc5d6b14
EC2 Default User <ec2-user@ip-10-0-31-153.us-west-2.compute.internal>
2024-01-19 18:27:56 +0000

    Update scripts folder for full workflow 	modified:   02_scripts/__pycache__/S01_specdiv_functions.cpython-38.pyc 	modified:   02_scripts/__pycache__/window_calcs.cpython-38.pyc 	modified:   02_scripts/clip_merge.py 	modified:   02_scripts/merge_clipped.py 	modified:   02_scripts/mosaic_flightlines.py 	modified:   02_scripts/window_calcs.py 	02_scripts/clip_SERC_flightlines.py 	02_scripts/clip_SRER_flightlines.py 	02_scripts/clip_mosaic.py 	02_scripts/fric_mosaic_tiff.py 	02_scripts/fric_mosaic_veg_mask.py 	02_scripts/merge_clip.py 	02_scripts/mosaic_S3.py 	02_scripts/mosaic_SERC_S3.py 	02_scripts/mosaic_SRER.py 	02_scripts/mosaic_SRER_S3.py 	02_scripts/mosaic_TEAK.py 	02_scripts/mosaic_clipped_lines_SRER.py 	02_scripts/mosaic_flightlines_SERC.py 	02_scripts/mosaic_flightlines_SRER.py

--- 02_scripts/window_calcs.py
+++ 02_scripts/window_calcs.py
@@ -15,12 +15,15 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 import kneed
 from kneed import KneeLocator
+import scipy.spatial
 from scipy.spatial import ConvexHull
 import subprocess
 from urllib.request import urlretrieve
 import parmap
 import os
 from tqdm import tqdm
+import csv
+from csv import writer
 
 def window_calcs(args):
     
@@ -38,21 +41,49 @@ def window_calcs(args):
     volume_mean: functional richness for given window size and image.
     
     """
-    windows, pca_chunk, results_FR = args
+    windows, pca_chunk, results_FR, local_file_path  = args
+    #print(pca_chunk[15:20, 30:40, 1])
+    window_data = []
     for window in tqdm(windows, desc='Processing window for batch'):
-        comps = 4
+        comps = 3
         half_window = window // 2
-        fric = np.zeros(pca_chunk.shape)
-        hull = None
-        for i in tqdm(range(half_window, pca_chunk.shape[0] - half_window), desc='Processing window index'):
-            for j in range(half_window, pca_chunk.shape[1] - half_window):
+        #print(pca_chunk.shape)
+        fric = np.zeros((pca_chunk.shape[0], pca_chunk.shape[1]))
+        for i in tqdm(range(half_window, pca_chunk.shape[0] - half_window, 15), desc='Processing window index'):
+            for j in range(half_window, pca_chunk.shape[1] - half_window, 15):
+                hull = None
                 sub_arr = pca_chunk[i - half_window:i + half_window + 1, j - half_window:j + half_window + 1, :]
+                #print(sub_arr.shape)
                 sub_arr = sub_arr.reshape((-1, comps))
+                #print(i,j)
+                #print(sub_arr.shape)
                 mean_arr = np.nanmean(sub_arr, axis=0)
+                #print(mean_arr)
                 non_zero_indices = np.nonzero(mean_arr)[0]
-                if len(non_zero_indices) >= 4:
-                    if hull is None:
-                        hull = ConvexHull(sub_arr[:, non_zero_indices])
-                    fric[i, j] = hull.volume
-        results_FR.append(np.nanmean(fric))
+                #print(non_zero_indices)
+                if len(non_zero_indices) >= 3:
+                    try:
+                        if hull is None:
+                            #print(sub_arr[:,non_zero_indices])
+                            hull = ConvexHull(sub_arr)
+                        #fric[i, j] = hull.volume
+                        #print(hull.volume)
+                        #print(fric)
+                    except scipy.spatial.qhull.QhullError as e:
+                        continue
+                window_data.append([window, hull.volume])
+        print(f"Hull volumes for window size {window}: {np.unique(fric)}")
+        #results_FR[window] = fric.tolist()
+
+        with open(local_file_path, 'a', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            if csvfile.tell() == 0:
+                csvwriter.writerow(['Window_Size', 'Hull_Volume'])  # Write header
+                                        
+           # for window_result, fric_matrix in results_FR.items():
+           #     for row in fric_matrix:
+           #         csvwriter.writerow([window_result] + row)
+            for data_point in window_data:
+                csvwriter.writerow(data_point)
+        #results_FR.append(np.nanmean(fric))
     return results_FR
