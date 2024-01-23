01309a54bd0565cddd2617055c0919a45863e723
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-01-23 13:15:35 -0700

    Outline script placement

--- 02_scripts/Workflow.py
+++ 02_scripts/Workflow.py
@@ -1,1 +1,26 @@
+# Import functions
+from S01_Functions import * 
 
+# Universal variables
+
+# Define NEON sites of interest
+NEON_sites = ['TEAK', 'SERC', 'SRER']
+
+# Pull flightlines for each site
+# Write code here to pull in a table and subset to sites of interest - called 'all_flights'
+
+## PROCESSING WORKFLOW ##
+
+# 1. Topo/BRDF corrections
+topo_brdf_correct(all_flights)
+print("Corrections complete")
+
+# 2. Clip to regions of interest
+
+# 3. Mosaic flightlines
+
+# 4. Compute Functional Richness
+
+# 5. Compute Functional Evenness
+
+# 6. Compute Functional Divergence
