1cdabc9e5692d859fcadd92f2869de4081350f6d
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-01-23 14:15:29 -0700

    Add additional processing steps

--- 02_scripts/Workflow.py
+++ 02_scripts/Workflow.py
@@ -1,7 +1,16 @@
 # Import functions
 from S01_Functions import * 
 
-# Universal variables
+# Set working directories
+Data_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/01_rawdata'
+Out_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
+bucket_name = 'bioscape.gra'
+s3 = boto3.client('s3')
+
+# Set global parameters
+window_sizes = [60, 120, 240, 480, 700, 960, 1200, 1500, 2000, 2200]
+ndvi_threshold = 0.4 # ndvi threshold for radiometric filtering
+comps = 3 # default component numbers for PCA
 
 # Define NEON sites of interest
 NEON_sites = ['TEAK', 'SERC', 'SRER']
@@ -17,11 +26,21 @@ topo_brdf_correct(all_flights)
 print("Corrections complete")
 
 # 2. Clip to regions of interest
+clip_flightline(all_lines)
+print("Flightlines Clipped")
 
 # 3. Mosaic flightlines
+mosaic_flightlines(clipped_lines)
+print("Flightlines mosaicked")
 
 # 4. Compute Functional Richness
+compute_fric(all_mosaics)
+print("FRic computed for all mosaics")
 
 # 5. Compute Functional Evenness
+compute_feve(all_mosaics)
+print("FEve computed for all mosaics")
 
 # 6. Compute Functional Divergence
+compute_fdiv(all_mosaics)
+print("FDiv computed for all mosaics")
