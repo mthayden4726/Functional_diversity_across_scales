d94712f064a31e6137d44a01ae7914fb444f4bb8
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-13 08:27:45 -0700

    Translate to python

--- 02_scripts/S05_Compute_FEve.py
+++ 02_scripts/S05_Compute_FEve.py
@@ -1,35 +1,43 @@
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+from scipy.sparse.csgraph import minimum_spanning_tree
 
-# In R:
-      # 2- Functional Divergence mean distance from centroid
-      if (!is.na(match('FDiv', FDmetric))){
-        vert1 <- scan("vert.txt", quiet = T)
-        vert2 <- vert1 + 1
-        vertices <- vert2[-1]
-        trvertices <- spectraits[vertices, ]
-        # coordinates of the center of gravity of the vertices (Gv)
-        baryv <- apply(trvertices, 2, mean)
-        # euclidian dstances to Gv (dB)
-        distbaryv <- rep(0, nbSpecies)
-        for (j in 1:nbSpecies) distbaryv[j] <- (sum((spectraits[j, ] - baryv)^2) ) ^0.5
-        # mean of dB values
-        meandB <- mean(distbaryv)
-        # deviations to mean of db
-        devdB <- distbaryv - meandB
-        # computation of FDiv
-        FDiv <- (sum(devdB/nbSpecies) + meandB) / (sum(abs(devdB/nbSpecies)) + meandB)
-      }
-    }
-    if (!is.na(match('FEve', FDmetric))){
-      # computation of minimum spanning tree and conversion of the 'mst' matrix into 'dist' class
-      tr.dist <- stats::dist(spectraits)
-      linkmst <- ape::mst(tr.dist)
-      mstvect <- as.dist(linkmst)
-      # computation of EW for the (nbSpecies - 1) segments to link the nbSpecies points
-      EW <- rep(0, nbSpecies - 1)
-      flag <- 1
-      for (m in 1 : ((nbSpecies - 1) * nbSpecies / 2)) {
-        if (mstvect[m] != 0) {
-          EW[flag] <- tr.dist[m]
-          flag <- flag + 1
-        }
-      }
+# Calculate functional divergence from PCA
+def calculate_FDiv_from_PCA(pca):
+    # Calculate mean of PCA data along the first axis
+    mean_pca = np.mean(pca, axis=0)
+    # Euclidean distances to mean along the first axis
+    dist_to_mean = np.linalg.norm(pca - mean_pca, axis=1)
+    # Mean of distances
+    meandB = np.mean(dist_to_mean)
+    # Deviations to mean
+    devdB = dist_to_mean - meandB
+    # Computation of FDiv
+    FDiv = (np.sum(devdB) / len(dist_to_mean) + meandB) / (np.sum(np.abs(devdB)) / len(dist_to_mean) + meandB)
+    return FDiv
+
+# Calculate functional evenness from PCA
+
+# First create vertex information (assign each row in PCA to a vertex)
+vertex_info = pca # pca must be an array
+nb = pca.shape[0]
+
+# Compute pairwise Euclidean distances between vertices
+distances = pdist(vertex_info)
+
+# Convert the condensed distance matrix to a square distance matrix
+dist_matrix = squareform(distances)
+
+# Compute minimum spanning tree
+mst = minimum_spanning_tree(dist_matrix)
+mstvect = mst.toarray()
+
+def calculate_FEve(mstvect, dist_matrix, nb):
+    # computation of EW for the (nb - 1) segments to link the nb points
+    EW = np.zeros(nb - 1)
+    flag = 0
+    for m in range((nb - 1) * nb // 2):
+        if mstvect[m] != 0:
+            EW[flag] = dist_matrix[m]
+            flag += 1
+    return EW
