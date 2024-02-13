242ca18812d170af6d90d75667c861fd944194a4
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-13 08:42:08 -0700

    Add parallel processing for FEve

--- 02_scripts/S05_Compute_FEve.py
+++ 02_scripts/S05_Compute_FEve.py
@@ -1,43 +1,91 @@
+## Import packages ##
+import hytools as ht
+import matplotlib.pyplot as plt
+import matplotlib.colors as clr
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
-from scipy.sparse.csgraph import minimum_spanning_tree
+import requests
+import sklearn
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import kneed
+from kneed import KneeLocator
+import scipy.spatial
+from scipy.spatial import ConvexHull
+import subprocess
+from urllib.request import urlretrieve
+import multiprocessing as mp
+import os, glob
+import csv
+import rasterio
+from osgeo import gdal
+import rioxarray as rxr
+import xarray as xr
+import earthpy as et
+import earthpy.spatial as es
+import earthpy.plot as ep
+import copy
+import re
+import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
+from shapely.geometry import box
+import geopandas as gpd
+import pandas as pd
+from fiona.crs import from_epsg
+import pycrs
+import csv
+from csv import writer
+# Import functions defined in other scripts
+from S01_Moving_Window_FEve.py import * # add scripts folder to python path manager
+from S01_Functions.py import * # add scripts folder to python path manager
 
-# Calculate functional divergence from PCA
-def calculate_FDiv_from_PCA(pca):
-    # Calculate mean of PCA data along the first axis
-    mean_pca = np.mean(pca, axis=0)
-    # Euclidean distances to mean along the first axis
-    dist_to_mean = np.linalg.norm(pca - mean_pca, axis=1)
-    # Mean of distances
-    meandB = np.mean(dist_to_mean)
-    # Deviations to mean
-    devdB = dist_to_mean - meandB
-    # Computation of FDiv
-    FDiv = (np.sum(devdB) / len(dist_to_mean) + meandB) / (np.sum(np.abs(devdB)) / len(dist_to_mean) + meandB)
-    return FDiv
+# Set directories
+Data_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
+Out_Dir = '/home/ec2-user/BioSCape_across_scales/03_output'
+bucket_name = 'bioscape.gra'
+s3 = boto3.client('s3')
 
-# Calculate functional evenness from PCA
+## Set global parameters ##
+window_sizes = [60, 120, 240, 480, 700, 960, 1200, 1500, 2000, 2200]
+ndvi_threshold = 0.4 # ndvi threshold for radiometric filtering
+comps = 3 # default component numbers for PCA
+# Other potential options (not currently included in this script):
+# shade_threshold = 500 # should be low threshold across NIR region (15%)
+# cloud_threshold = 1500 # should be high threshold in blue band
+# bad_bands = [[300,400],[1300,1450],[1780,2000],[2450,2600]] # bands to be masked out
+# sample_size = 0.1 # proportion of pixels to subsample for fitting PCA
+# nclusters = 15 # default component numbers for K-means clustering
 
-# First create vertex information (assign each row in PCA to a vertex)
-vertex_info = pca # pca must be an array
-nb = pca.shape[0]
-
-# Compute pairwise Euclidean distances between vertices
-distances = pdist(vertex_info)
-
-# Convert the condensed distance matrix to a square distance matrix
-dist_matrix = squareform(distances)
-
-# Compute minimum spanning tree
-mst = minimum_spanning_tree(dist_matrix)
-mstvect = mst.toarray()
-
-def calculate_FEve(mstvect, dist_matrix, nb):
-    # computation of EW for the (nb - 1) segments to link the nb points
-    EW = np.zeros(nb - 1)
-    flag = 0
-    for m in range((nb - 1) * nb // 2):
-        if mstvect[m] != 0:
-            EW[flag] = dist_matrix[m]
-            flag += 1
-    return EW
+# Loop through clipped files
+file_stem = 'SRER_flightlines/Mosaic_SRER_shape_'
+sites = [3,4]
+for i in sites:
+    clip_file = file_stem + str(i) + '.tif'
+    print(clip_file)
+    s3.download_file(bucket_name, clip_file, Data_Dir + '/mosaic.tif')
+    file = Data_Dir + '/mosaic.tif'
+    raster = rxr.open_rasterio(file, masked=True)
+    print(raster)
+    # Array-ize, transform with PCA
+    pca_x = pca_steps(raster, comps)
+    # Paralellize calcs for different window sizes
+    results_FE = {}
+    local_file_path = Out_Dir + "/SRER_feve_" + str(i) + ".csv"
+    window_batches = [(a, pca_x, results_FE, local_file_path) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
+    volumes = process_map(
+        window_calcs_feve,
+        window_batches,
+        max_workers=cpu_count() - 1
+    )
+    #print(volumes)
+    # open file for writing
+    # local_file_path = Out_Dir + "/TEAK_fric_" + str(i) + ".csv"
+    destination_s3_key = "/SRER_feve_" + str(i) + ".csv"
+    #f = open(local_file_path,"w")
+    # write file
+    #f.write(str(volumes))
+    # close file
+    #f.close()
+    upload_to_s3(bucket_name, local_file_path, destination_s3_key)
+    print("File uploaded to S3")
+    os.remove(file)
+    print("Mosaic Complete - Next...")
