25273e8ed809ea27115a01aa19cb9f48bc7415dd
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-15 13:06:46 -0700

    Start updating for FEve

--- 02_scripts/S01_Moving_Window_FDiv.py
+++ 02_scripts/S01_Moving_Window_FDiv.py
@@ -23,20 +23,9 @@ def calculate_FDiv_from_PCA(pca):
     return FDiv
 
 
-## OLD CODE FOR FEVE - NEED TO UPDATE BELOW ##
-
-def calculate_FEve(mstvect, dist_matrix, nb):
-    EW = np.zeros(nb - 1)
-    flag = 0
-    for m in range((nb - 1) * nb // 2):
-        if mstvect[m] != 0:
-            EW[flag] = dist_matrix[m]
-            flag += 1
-    return EW
-  
-def window_calcs_feve(args):
+def window_calcs_fdiv(args):
     
-    """ Calculate functional evenness using MST for a single PCA chunk and window size.
+    """ Calculate functional divergence using a PCA chunk and window size.
     FOR USE IN PARALLEL PROCESSING OF FUNCTIONAL EVENNESS.
     
     Parameters:
@@ -46,23 +35,24 @@ def window_calcs_feve(args):
     
     Returns:
     -----------
-    FEve: functional evenness for given window size and image.
+    FDiv: functional divergence for given window size and image.
     
     """
-    windows, pca_chunk, results_FR, local_file_path  = args
+    windows, pca_x, results_FD, local_file_path  = args
     for window in tqdm(windows, desc='Processing window for batch'):
+        print(window)
         half_window = window // 2
-        FEve_values = []
-        for i in tqdm(range(half_window, pca_chunk.shape[0] - half_window, 15), desc='Processing window index'):
-            for j in range(half_window, pca_chunk.shape[1] - half_window, 15):
-                sub_arr = pca_chunk[i - half_window:i + half_window + 1, j - half_window:j + half_window + 1, :]
-                nbSpecies = sub_arr.shape[0] * sub_arr.shape[1]
-                distances = pdist(sub_arr.reshape(nbSpecies, -1))
-                dist_matrix = squareform(distances)
-                mst = minimum_spanning_tree(dist_matrix)
-                mstvect = mst.toarray().flatten()
-                FEve = calculate_FEve(mstvect, dist_matrix, nbSpecies)
-                FEve_values.extend(FEve)
+        FDiv_values = []
+        for i in tqdm(range(half_window, pca_x.shape[0] - half_window, 15), desc='Processing window index'):
+            for j in range(half_window, pca_x.shape[1] - half_window, 15):
+                sub_arr = pca_x[i - half_window:i + half_window + 1, j - half_window:j + half_window + 1, :]
+                if sub_arr is None:
+                    print(f"sub_arr is None at position ({i}, {j})")
+                    continue  # Skip this iteration if sub_arr is None
+                print(f"sub_arr shape: {sub_arr.shape}")
+                FDiv = calculate_FDiv(sub_arr)
+                print(FDiv)
+                FDiv_values.extend(FDiv)
                 
         with open(local_file_path, 'a', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
@@ -70,5 +60,3 @@ def window_calcs_feve(args):
                 csvwriter.writerow(['Window_Size', 'FEve'])  # Write header
             for FEve_value in FEve_values:
                 csvwriter.writerow([window, FEve_value])
-
-    
