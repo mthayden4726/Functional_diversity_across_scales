dd7e18138dd58093139b114dc91e76dc6b48ce22
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-21 14:56:40 -0700

    Update trial_find_files.py

--- 02_scripts/trial_find_files.py
+++ 02_scripts/trial_find_files.py
@@ -3,4 +3,18 @@ SITECODE = 'HEAL'
 PRODUCTCODE = 'DP1.30006.001'
 YEAR = '2019'
 
-find_neon_files(SITECODE, PRODUCTCODE, YEAR)
+SERVER = 'http://data.neonscience.org/api/v0/'
+# Build so that we can loop through reading files in
+url = SERVER+'data/'+PRODUCTCODE+'/'+SITECODE+'/'+YEAR
+# Request the url
+data_request = requests.get(url)
+# Convert the request to Python JSON object
+data_json = data_request.json()
+print(data_json)
+# Create list of file paths of interest for given site, product, year
+file_paths = []
+for file in data_json['data']['files'][:20]:
+  if 'reflectance.h5' in file['name']:
+    file_paths.insert(1, file['url'])
+    print(file['url'])
+  return file_paths
