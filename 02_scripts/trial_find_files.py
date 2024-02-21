a324a9b0efde0afdc2069acf0b8d8c921d1de364
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-21 14:58:27 -0700

    Add month

--- 02_scripts/trial_find_files.py
+++ 02_scripts/trial_find_files.py
@@ -1,7 +1,7 @@
 from S01_Functions import * # add scripts folder to python path manager
 SITECODE = 'HEAL'
 PRODUCTCODE = 'DP1.30006.001'
-YEAR = '2019'
+YEAR = '2019-06'
 
 SERVER = 'http://data.neonscience.org/api/v0/'
 # Build so that we can loop through reading files in
