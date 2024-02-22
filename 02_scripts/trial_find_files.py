013812ddf469471109dbd4641f650e29d1e8a0e8
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-22 13:26:51 -0700

    Find KONZ numbers

--- 02_scripts/trial_find_files.py
+++ 02_scripts/trial_find_files.py
@@ -1,7 +1,7 @@
 from S01_Functions import * # add scripts folder to python path manager
-SITECODE = 'HEAL'
+SITECODE = 'KONZ'
 PRODUCTCODE = 'DP1.30006.001'
-YEAR = '2019-08'
+YEAR = '2019-05'
 
 SERVER = 'http://data.neonscience.org/api/v0/'
 # Build so that we can loop through reading files in
