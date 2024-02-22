c797a24107a022d039b6d6daad27d7c9e67d8647
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-21 19:54:13 -0700

    Fix typo, remove .py from script import

--- 02_scripts/S02_TopoBRDF_Corrections_HEAL.py
+++ 02_scripts/S02_TopoBRDF_Corrections_HEAL.py
@@ -24,7 +24,7 @@ import tqdm
 from progress.bar import Bar
 from tqdm.contrib.concurrent import process_map
 from multiprocessing import Pool, cpu_count
-from S01_Moving_Window_FRIC.py import * # add scripts folder to python path manager
+from S01_Moving_Window_FRIC import * # add scripts folder to python path manager
 from S01_Functions import * # add scripts folder to python path manager
 from osgeo import gdal, osr
 from matplotlib.pyplot import subplots, show
