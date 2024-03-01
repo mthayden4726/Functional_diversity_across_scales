c1559361912bab7504a4aae1f80825b561a30bf8
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-29 17:46:42 -0700

    Try with masker

--- 02_scripts/S02_TopoBRDF_Corrections_Auto.py
+++ 02_scripts/S02_TopoBRDF_Corrections_Auto.py
@@ -42,12 +42,18 @@ bucket_name = 'bioscape.gra'
 s3 = boto3.client('s3')
 
 # Select site
-site = "HARV"
+site = "TALL"
 
 # Set parameters for NDVI threshold
 nir_band = 90
 red_band = 58
-ndvi_threshold = 0.4
+ndvi_threshold = 0.3
+
+def masker(hy_obj):
+    ir = hy_obj.get_band(90)
+    red = hy_obj.get_band(58)
+    ndvi = (ir-red)/(ir+red)
+    return ndvi > .3
 
 # Find correction coefficients (define search terms) - listing first dates for each for now
 if site == "HARV":
@@ -72,6 +78,7 @@ elif site == "TALL":
   date_id = '2019042713'
   site_id = '2019_TALL_5'
   domain = 'D08'
+  epsg = 32616
 elif site == "WOOD":
   search_criteria = 'NEON_D09_WOOD_DP1_20190722'
   date_id = '2019072214'
@@ -168,6 +175,8 @@ for i,file in enumerate(file_names):
     brdf_coeffs = Data_Dir + "/brdf.json"
     neon.load_coeffs(topo_coeffs,'topo')
     neon.load_coeffs(brdf_coeffs, 'brdf')
+    # Generate mask
+    neon.gen_mask(masker)
     print("corrections loaded")
     # Store map info for raster
     refl_md, header_dict = store_metadata(neon, epsg)
@@ -176,20 +185,21 @@ for i,file in enumerate(file_names):
     wavelength = header_dict['wavelength']
     good_wl = np.where((wavelength < 1340) | (wavelength > 1955), wavelength, np.nan)
     good_wl_list = good_wl[~np.isnan(good_wl)]
+    reflClean[nodata_ind]=np.nan
     print("creating arrays")
-    arrays = [neon.get_wave(wave, corrections= ['topo','brdf'], mask = None) for wave in good_wl_list]
+    arrays = [neon.get_wave(wave, corrections= ['topo','brdf'], mask_values = True) for wave in good_wl_list]
     print("stacking arrays")
     fullarraystack = np.dstack(arrays)
     print("Shape of fullarraystack:", fullarraystack.shape)
     print("calculating ndvi")
-    ndvi = np.divide((fullarraystack[:, :, nir_band] - fullarraystack[:, :, red_band]), (fullarraystack[:, :, nir_band] + fullarraystack[:, :, red_band]), 
-                     where=(fullarraystack[:, :, nir_band] + fullarraystack[:, :, red_band]) != 0)
-    print("Shape of ndvi array:", ndvi.shape)
+    #ndvi = np.divide((fullarraystack[:, :, nir_band] - fullarraystack[:, :, red_band]), (fullarraystack[:, :, nir_band] + fullarraystack[:, :, red_band]), 
+    #                 where=(fullarraystack[:, :, nir_band] + fullarraystack[:, :, red_band]) != 0)
+    #print("Shape of ndvi array:", ndvi.shape)
     # Apply NDVI threshold mask
-    mask = ndvi < ndvi_threshold
-    print("Shape of mask array:", mask.shape)
-    print("masking by ndvi")
-    fullarraystack[mask, :] = np.nan
+    #mask = ndvi < ndvi_threshold
+    #print("Shape of mask array:", mask.shape)
+    #print("masking by ndvi")
+    #fullarraystack[mask, :] = np.nan
     destination_s3_key = site + '_flightlines/'+ str(file)+'_output_' + '.tif'
     local_file_path = Out_Dir + '/output_fullarray_' + file + '.tif'
     print(local_file_path)
