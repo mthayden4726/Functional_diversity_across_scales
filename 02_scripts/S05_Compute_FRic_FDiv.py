7efbfa6561836a36ced941ed024356c7b20026c3
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-02-29 17:51:47 -0700

    Try with ONAQ

--- 02_scripts/S05_Compute_FRic_FDiv.py
+++ 02_scripts/S05_Compute_FRic_FDiv.py
@@ -47,16 +47,13 @@ s3 = boto3.client('s3')
 ## Set global parameters ##
 # window_sizes = [10, 30, 60, 120]   # list of window sizes to test
 window_sizes = [60, 120, 240, 480, 960, 1200, 1500, 2000, 2200]
-red_band = 58
-nir_band = 90
-ndvi_threshold = 0.5 # ndvi threshold for radiometric filtering
 comps = 3 # default component numbers for PCA
 
 # Loop through clipped files
 # Choose site and plots
-file_stem = 'HEAL_flightlines/Mosaic_HEAL_'
-plots = ['005','015', '018', '024', '026']
-# go back and do '005' for just fdiv
+file_stem = 'ONAQ_flightlines/Mosaic_ONAQ_'
+plots = ['002']
+
 # Loop through plots
 for i in plots:
     clip_file = file_stem + str(i) + '.tif'
@@ -79,10 +76,12 @@ for i in plots:
     print(X.shape)
     X = X.astype('float32')
     X[np.isnan(X)] = np.nan
+    X[X < 0] = np.nan
     X = X/10000 # rescale data
     # Change nan to 0 
-    X_no_nan = np.nan_to_num(X, nan=0)
+    #X_no_nan = np.nan_to_num(X, nan=0)
     # Take mean 
+    X_no_nan = X
     x_mean = X_no_nan.mean(axis=0)[np.newaxis, :]
     # Scale & standardize array
     X -=x_mean
@@ -98,14 +97,14 @@ for i in plots:
     print(pca_x.shape)
     # Calculate FRic on PCA across window sizes
     results_FR = {}
-    local_file_path_fric = Out_Dir + "/HEAL_fric_" + str(i) + ".csv"
+    local_file_path_fric = Out_Dir + "/ONAQ_fric_" + str(i) + ".csv"
     window_batches = [(a, pca_x, results_FR, local_file_path) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs,
         window_batches,
         max_workers=cpu_count() - 1
     )
-    destination_s3_key_fric = "/HEAL_fric_veg_" + str(i) + ".csv"
+    destination_s3_key_fric = "/ONAQ_fric_veg_" + str(i) + ".csv"
     #f = open(local_file_path,"w")
     # write file
     #f.write(str(volumes))
@@ -115,7 +114,7 @@ for i in plots:
     print("FRic file uploaded to S3")
     # Calculate FDiv on PCA across window sizes
     results_FD = {}
-    local_file_path_fdiv = Out_Dir + "/HEAL_fdiv_veg_" + str(i) + ".csv"
+    local_file_path_fdiv = Out_Dir + "/ONAQ_fdiv_veg_" + str(i) + ".csv"
     window_batches = [(a, pca_x, results_FD, local_file_path_fdiv) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs_fdiv,
@@ -123,7 +122,7 @@ for i in plots:
         max_workers=cpu_count() - 1
     )
     # open file for writing
-    destination_s3_key_fdiv = "/HEAL_fdiv_veg_" + str(i) + ".csv"
+    destination_s3_key_fdiv = "/ONAQ_fdiv_veg_" + str(i) + ".csv"
     upload_to_s3(bucket_name, local_file_path_fdiv, destination_s3_key_fdiv)
     print("FDiv file uploaded to S3")
     os.remove(file)
