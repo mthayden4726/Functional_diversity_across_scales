810106532cca49bfaa8dd96c6d7e053a3693a3a1
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-02 12:07:33 -0700

    Update local file path name

--- 02_scripts/S05_Compute_FRic_FDiv.py
+++ 02_scripts/S05_Compute_FRic_FDiv.py
@@ -100,7 +100,7 @@ for i in plots:
     # Calculate FRic on PCA across window sizes
     results_FR = {}
     local_file_path_fric = Out_Dir + "/ONAQ_fric_" + str(i) + ".csv"
-    window_batches = [(a, pca_x, results_FR, local_file_path) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
+    window_batches = [(a, pca_x, results_FR, local_file_path_fric) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs,
         window_batches,
