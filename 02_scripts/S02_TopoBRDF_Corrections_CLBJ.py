a00a0469cdbad9adfa3f92e7b8633796325c6d02
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-05 10:24:45 -0700

    Update and rename S02_TopoBRDF_Corrections_OSBS.py to S02_TopoBRDF_Corrections_CLBJ.py

--- 02_scripts/S02_TopoBRDF_Corrections_CLBJ.py
+++ 02_scripts/S02_TopoBRDF_Corrections_CLBJ.py
@@ -44,11 +44,11 @@ s3 = boto3.client('s3')
 nir_band = 90
 red_band = 58
 ndvi_threshold = 0.25
-epsg = 32617
+epsg = 32614
 
 # Find correction coefficients (define search terms)
-search_criteria = "NEON_D03_OSBS_DP1_20190422"
-dirpath = "NEON BRDF-TOPO Corrections/2019_OSBS/"
+search_criteria = "NEON_D11_CLBJ_DP1_20190419"
+dirpath = "NEON BRDF-TOPO Corrections/2019_CLBJ/"
 
 # List objects in the S3 bucket in the matching directory
 objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
@@ -67,7 +67,7 @@ file_names = list(file_names)  # Convert set back to a list if needed
 print(file_names)
 
 
-# Loop through all OSBS files
+# Loop through all CLBJ files
 for i,file in enumerate(file_names):
 
     # Set to none to reduce memory use
@@ -86,20 +86,20 @@ for i,file in enumerate(file_names):
     mask = None
     
     print(file)
-    flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/D03/2019_OSBS_5/L1/Spectrometer/ReflectanceH5/2019042213/NEON_D03_OSBS_DP1_' + file +'_reflectance.h5'
+    flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/D11/2019_CLBJ_4/L1/Spectrometer/ReflectanceH5/2019041914/NEON_D11_CLBJ_DP1_' + file +'_reflectance.h5'
     files = []
     files.append(flight)
     try:
         retrieve_neon_files(files, Data_Dir)
     except Exception as e:
         continue 
-    img = Data_Dir + "/NEON_D03_OSBS_DP1_" + file + '_reflectance.h5'
+    img = Data_Dir + "/NEON_D11_CLBJ_DP1_" + file + '_reflectance.h5'
     neon = ht.HyTools() 
     neon.read_file(img,'neon')
     print("file loaded")
-    topo_file = "NEON BRDF-TOPO Corrections/2019_OSBS/NEON_D03_OSBS_DP1_" + file + "_reflectance_topo_coeffs_topo.json"
+    topo_file = "NEON BRDF-TOPO Corrections/2019_CLBJ/NEON_D11_CLBJ_DP1_" + file + "_reflectance_topo_coeffs_topo.json"
     print(topo_file)
-    brdf_file = "NEON BRDF-TOPO Corrections/2019_OSBS/NEON_D03_OSBS_DP1_" + file + "_reflectance_brdf_coeffs_topo_brdf.json"
+    brdf_file = "NEON BRDF-TOPO Corrections/2019_CLBJ/NEON_D11_CLBJ_DP1_" + file + "_reflectance_brdf_coeffs_topo_brdf.json"
     s3.download_file(bucket_name, topo_file, Data_Dir + '/topo.json')
     s3.download_file(bucket_name, brdf_file, Data_Dir + '/brdf.json')
     print("Files downloaded successfully.")
@@ -128,7 +128,7 @@ for i,file in enumerate(file_names):
     print("Shape of mask array:", mask.shape)
     print("masking by ndvi")
     fullarraystack[mask, :] = np.nan
-    destination_s3_key = 'OSBS_flightlines/'+ str(file)+'_output_' + '.tif'
+    destination_s3_key = 'CLBJ_flightlines/'+ str(file)+'_output_' + '.tif'
     local_file_path = Out_Dir + '/output_fullarray_' + file + '.tif'
     print(local_file_path)
     print("rasterizing array")
