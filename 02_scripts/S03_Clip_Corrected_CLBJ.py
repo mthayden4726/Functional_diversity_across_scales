82033e5446b2611f1bb67d4e45eb217fbfee87d3
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-05 18:56:33 -0700

    Update and rename S03_Clip_Corrected_TALL.py to S03_Clip_Corrected_CLBJ.py

--- 02_scripts/S03_Clip_Corrected_CLBJ.py
+++ 02_scripts/S03_Clip_Corrected_CLBJ.py
@@ -26,7 +26,7 @@ s3 = boto3.client('s3')
 
 # Find files for mosaicing (define search terms)
 search_criteria = "201904"
-dirpath = "TALL_flightlines/"
+dirpath = "CLBJ_flightlines/"
 
 # List objects in the S3 bucket in the matching directory
 objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
@@ -35,16 +35,16 @@ files = [obj['Key'] for obj in objects if obj['Key'].endswith('.tif') and (searc
 print(files)
 
 # List shapefile prefices
-shapefiles = ['Site_boundaries/TALL/TALL_001',
-              'Site_boundaries/TALL/TALL_002',
-              'Site_boundaries/TALL/TALL_012',
-              'Site_boundaries/TALL/TALL_013',
-              'Site_boundaries/TALL/TALL_009',
-              'Site_boundaries/TALL/TALL_020',
-              'Site_boundaries/TALL/TALL_044',
-              'Site_boundaries/TALL/TALL_026',
-              'Site_boundaries/TALL/TALL_027',
-              'Site_boundaries/TALL/TALL_032'
+shapefiles = ['Site_boundaries/CLBJ/CLBJ_003'
+              'Site_boundaries/CLBJ/CLBJ_035',
+              'Site_boundaries/CLBJ/CLBJ_041',
+              'Site_boundaries/CLBJ/CLBJ_053',
+              'Site_boundaries/CLBJ/CLBJ_054',
+              'Site_boundaries/CLBJ/CLBJ_048',
+              'Site_boundaries/CLBJ/CLBJ_047',
+              'Site_boundaries/CLBJ/CLBJ_050',
+              'Site_boundaries/CLBJ/CLBJ_051',
+              'Site_boundaries/CLBJ/CLBJ_056'
              ]
 
 # Load the polygon for clipping ()
@@ -83,7 +83,7 @@ for j,shape in enumerate(shapefiles):
                 with rasterio.open(local_file_path, "w", **out_meta) as dest:
                     dest.write(out_image)
                     print("File Written")
-                destination_s3_key = 'TALL_flightlines/' + str(shape) + '_Clipped_file_' + str(i) + '.tif'
+                destination_s3_key = 'CLBJ_flightlines/' + str(shape) + '_Clipped_file_' + str(i) + '.tif'
                 upload_to_s3(bucket_name, local_file_path, destination_s3_key)
                 print("File uploaded to S3")
                 os.remove(local_file_path)
