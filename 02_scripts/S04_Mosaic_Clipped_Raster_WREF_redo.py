eb41c6f39611e0704e1b828b826cd3e3498e9cc9
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-19 16:55:35 +0900

    Update S04_Mosaic_Clipped_Raster_WREF_redo.py

--- 02_scripts/S04_Mosaic_Clipped_Raster_WREF_redo.py
+++ 02_scripts/S04_Mosaic_Clipped_Raster_WREF_redo.py
@@ -1,97 +1,127 @@
-
-# Script to pull rasters from S3, clip them to the extent for the field sites, and then re-export them to S3.
-# Next they will be pulled in to be merged. 
-# Meghan Hayden - 2/22/2024
-
 import rasterio
 from rasterio.merge import merge
 from rasterio.plot import show
 from rasterio.mask import mask
-import fiona
+from rasterio.warp import transform_bounds
+from rasterio.transform import from_origin
+from rasterio.warp import calculate_default_transform, reproject
 import glob
 import os
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 from shapely.geometry import box
 import geopandas as gpd
+import fiona
 from fiona.crs import from_epsg
 import pycrs
 from osgeo import gdal
-from S01_Functions import *
+import numpy as np
+from S01_Functions import * # add scripts folder to python path manager
 
 Data_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/01_rawdata'
 Out_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
 bucket_name = 'bioscape.gra'
 s3 = boto3.client('s3')
 
-# Find files for mosaicing (define search terms)
-search_criteria = "201907"
-dirpath = "YELL_flightlines/"
+# Set config options
+gdal.SetConfigOption('SHAPE_RESTORE_SHX', 'YES')
+gdal.SetConfigOption('CHECK_DISK_FREE_SPACE', 'FALSE')
 
-# List objects in the S3 bucket in the matching directory
-objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
-# Filter objects based on the search criteria
-files = [obj['Key'] for obj in objects if obj['Key'].endswith('.tif') and (search_criteria in obj['Key'])]
-print(files)
+src_files_to_mosaic = []
 
-# List shapefile prefices
-shapefiles = [
-              'Site_boundaries/YELL/YELL_015',
-  'Site_boundaries/YELL/YELL_021',
-  'Site_boundaries/YELL/YELL_022',
-  'Site_boundaries/YELL/YELL_024',
-  'Site_boundaries/YELL/YELL_013',
-  'Site_boundaries/YELL/YELL_005',
-  'Site_boundaries/YELL/YELL_026',
-  'Site_boundaries/YELL/YELL_014',
-  'Site_boundaries/YELL/YELL_012',
-  'Site_boundaries/YELL/YELL_019',
-  'Site_boundaries/YELL/YELL_003',
-  'Site_boundaries/YELL/YELL_046',
-  'Site_boundaries/YELL/YELL_011'
-]
+file_ID = [
+           #'015',
+           '021',
+           '022',
+           '024',
+           '013',
+           '005',
+           '026',
+           '014',
+           '012',
+           '019',
+           '003',
+           '046',
+           '011'
+           ]
 
-# Load the polygon for clipping ()
-for j,shape in enumerate(shapefiles):
-    print(shape)
-    downloaded_files = download_shapefile(bucket_name, shape, Out_Dir)
-    shapefile_path = next(file for file in downloaded_files if file.endswith('.shp'))
-    
-    # Open shapefile and access geometry
-    with fiona.open(shapefile_path, "r") as shapefile:
-        shapes = [feature["geometry"] for feature in shapefile]
-    print(shapes)
-    #minx, miny, maxx, maxy = box(*shapes[0].bounds).bounds
-    
-    # Access the bounds of the entire GeoDataFrame
-    #gdf = gpd.read_file(shapefile_path)
-    #minx, miny, maxx, maxy = gdf.total_bounds
-    
-    for i, file in enumerate(files):
-        print('Loading file from S3')
-        s3.download_file(bucket_name, file, Out_Dir + '/file_' + str(i) + '.tif')
-        flight = Out_Dir + '/file_' + str(i) + '.tif'
-        print(flight)
+for i,ID in enumerate(file_ID):
+    src_files_to_mosaic = []
+    # List files associated with a single buffer shape
+    search_criteria = str(ID)
+    dirpath = "YELL_flightlines/Site_boundaries/YELL/"
+
+    # List objects in the S3 bucket in the matching directory
+    objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
+    # Filter objects based on the search criteria
+    files = [obj['Key'] for obj in objects if obj['Key'].endswith('.tif') and (search_criteria in obj['Key'])]
+    print(files)
+    for j,file in enumerate(files):
+        flight  = Out_Dir + '/file_' + str(j) + '.tif'
+        try:
+            s3.download_file(bucket_name, file, flight)
+            print(f"The file '{file}' exists.")
+        except Exception as e:
+            print(f"Error: {e}")
+
         with rasterio.open(flight) as src:
-            try:
-                out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
-                out_meta = src.meta
-                print("File Clipped")
-                print(out_meta)
-                out_meta.update({"driver": "GTiff",
-                    "height": out_image.shape[1],
-                    "width": out_image.shape[2],
-                    "transform": out_transform})
-                local_file_path = Out_Dir + "/clip.tif"
-                with rasterio.open(local_file_path, "w", **out_meta) as dest:
-                    dest.write(out_image)
-                    print("File Written")
-                destination_s3_key = 'YELL_flightlines/' + str(shape) + '_Clipped_file_' + str(i) + '.tif'
-                upload_to_s3(bucket_name, local_file_path, destination_s3_key)
-                print("File uploaded to S3")
-                os.remove(local_file_path)
-            except ValueError as e:
-                # Handle the case where there is no overlap between the raster and the shapefiles
-                print(f"Skipping file {i} as it does not overlap with the shapefile.")
-            os.remove(flight)
-        print("Cleared data files")
+            # Read raster as array
+            array = src.read()
+            # Convert nodata values to 0 (assumes nodata values are set correctly in the metadata)
+            nodata = src.nodata
+            print(nodata)
+            if nodata is not None:
+                array[array == nodata] = 0
+                array[array == None] = 0
+                array[np.isnan(array) | np.isinf(array)] = 0
+            else:
+                # Consider what to do if nodata is not defined, or define a default action
+                array[array == None] = 0
+                array[np.isnan(array) | np.isinf(array)] = 0
+                print("nodata is not defined")
+
+            # Define modified file path
+            modified_flight = Out_Dir + '/modified_file_' + str(j) + '.tif'
+
+            # Update metadata for the modified file
+            out_meta = src.meta.copy()
+            out_meta.update({
+                "nodata": 0  # Ensure nodata is now set to 0 in the metadata
+            })
+
+            # Write modified array to new TIFF
+            with rasterio.open(modified_flight, "w", **out_meta) as dest:
+                dest.write(array)
+        modified_src = rasterio.open(modified_flight)
+        src_files_to_mosaic.append(modified_src)
+
+    # Mosaic files
+    print(src_files_to_mosaic)
+    mosaic, out_trans = merge(src_files_to_mosaic, method = 'max')
+    print('Merge complete')
+    # Update metadata
+    out_meta = src.meta.copy()
+    print(out_meta)
+    print(mosaic.shape)
+    out_meta.update({"driver": "GTiff",
+        "height": mosaic.shape[1],
+        "width": mosaic.shape[2],
+        "transform": out_trans,
+        "crs": "+init=epsg:32612 +units=m +no_defs "}) # for TALL UTM WGS 16N
+    print(out_meta)
+
+    # Write to computer, send to S3
+    local_file_path = Out_Dir + "/mosaic_YELL.tif"
+    with rasterio.open(local_file_path, "w", **out_meta) as dest:
+        dest.write(mosaic)
+    print("File written")
+    
+    # Push to S3 bucket
+    destination_s3_key = 'YELL_flightlines/Mosaic_YELL_'+str(ID)+'.tif'
+    upload_to_s3(bucket_name, local_file_path, destination_s3_key)
+    print("File uploaded to S3")
+    
+    # Remove unneeded files (mosaic and shapefile)
+    os.remove(local_file_path)
+    mosaic = None
+    src_files_to_mosaic = None 
