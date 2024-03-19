1fdb5c1ae74b37b381a0620ea317eba17c59bf0d
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-19 16:47:22 +0900

    Run for WREF

--- 02_scripts/S05_Compute_FRic_FDiv.py
+++ 02_scripts/S05_Compute_FRic_FDiv.py
@@ -53,13 +53,17 @@ comps = 3 # default component numbers for PCA
 
 # Loop through clipped files
 # Choose site and plots
-file_stem = 'NIWO_flightlines/Mosaic_NIWO_'
-plots = ['030',
-         '004',
-         '007',
-         '021',
-         '016',
-        '006']
+file_stem = 'WREF_flightlines/Mosaic_WREF_'
+plots = ['029',
+  '070',
+  '002',
+  '012',
+  '020',
+  '006',
+  '026',
+  '015',
+          '023',
+          '021']
 
 # Loop through plots
 for i in plots:
@@ -105,14 +109,14 @@ for i in plots:
     print(pca_x.shape)
     # Calculate FRic on PCA across window sizes
     results_FR = {}
-    local_file_path_fric = Out_Dir + "/NIWO_fric_" + str(i) + ".csv"
+    local_file_path_fric = Out_Dir + "/WREF_fric_" + str(i) + ".csv"
     window_batches = [(a, pca_x, results_FR, local_file_path_fric) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs,
         window_batches,
         max_workers=cpu_count() - 1
     )
-    destination_s3_key_fric = "/NIWO_fric_veg_" + str(i) + ".csv"
+    destination_s3_key_fric = "/WREF_fric_veg_" + str(i) + ".csv"
     #f = open(local_file_path,"w")
     # write file
     #f.write(str(volumes))
@@ -122,7 +126,7 @@ for i in plots:
     print("FRic file uploaded to S3")
     # Calculate FDiv on PCA across window sizes
     results_FD = {}
-    local_file_path_fdiv = Out_Dir + "/NIWO_fdiv_veg_" + str(i) + ".csv"
+    local_file_path_fdiv = Out_Dir + "/WREF_fdiv_veg_" + str(i) + ".csv"
     window_batches = [(a, pca_x, results_FD, local_file_path_fdiv) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs_fdiv,
@@ -130,7 +134,7 @@ for i in plots:
         max_workers=cpu_count() - 1
     )
     # open file for writing
-    destination_s3_key_fdiv = "/NIWO_fdiv_veg_" + str(i) + ".csv"
+    destination_s3_key_fdiv = "/WREF_fdiv_veg_" + str(i) + ".csv"
     upload_to_s3(bucket_name, local_file_path_fdiv, destination_s3_key_fdiv)
     print("FDiv file uploaded to S3")
     os.remove(file)
