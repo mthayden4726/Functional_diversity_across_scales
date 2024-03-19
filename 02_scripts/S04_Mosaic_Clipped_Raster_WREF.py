c0fd5d26bb015d3287300d75676d1f5b97ca51c9
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-19 15:34:03 +0900

    Update and rename S04_Mosaic_Clipped_Raster_OSBS.py to S04_Mosaic_Clipped_Raster_WREF.py

--- 02_scripts/S04_Mosaic_Clipped_Raster_WREF.py
+++ 02_scripts/S04_Mosaic_Clipped_Raster_WREF.py
@@ -29,20 +29,22 @@ gdal.SetConfigOption('CHECK_DISK_FREE_SPACE', 'FALSE')
 
 src_files_to_mosaic = []
 
-file_ID = ['002',
-  '005',
-  '007',
-  '010',
-  '011',
-  '027',
-  '048',
-  '051']
+file_ID = ['029',
+  '070',
+  '002',
+  '012',
+  '020',
+  '006',
+  '026',
+  '015',
+          '023',
+          '021']
 
 for i,ID in enumerate(file_ID):
     src_files_to_mosaic = []
     # List files associated with a single buffer shape
     search_criteria = str(ID)
-    dirpath = "OSBS_flightlines/Site_boundaries/OSBS/"
+    dirpath = "WREF_flightlines/Site_boundaries/WREF/"
 
     # List objects in the S3 bucket in the matching directory
     objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
@@ -71,17 +73,17 @@ for i,ID in enumerate(file_ID):
         "height": mosaic.shape[1],
         "width": mosaic.shape[2],
         "transform": out_trans,
-        "crs": "+init=epsg:32617 +units=m +no_defs "}) # for OSBS UTM WGS 17N
+        "crs": "+init=epsg:32610 +units=m +no_defs "}) # for OSBS UTM WGS 17N
     print(out_meta)
 
     # Write to computer, send to S3
-    local_file_path = Out_Dir + "/mosaic_OSBS.tif"
+    local_file_path = Out_Dir + "/mosaic_WREF.tif"
     with rasterio.open(local_file_path, "w", **out_meta) as dest:
         dest.write(mosaic)
     print("File written")
     
     # Push to S3 bucket
-    destination_s3_key = 'OSBS_flightlines/Mosaic_OSBS_'+str(ID)+'.tif'
+    destination_s3_key = 'WREF_flightlines/Mosaic_WREF_'+str(ID)+'.tif'
     upload_to_s3(bucket_name, local_file_path, destination_s3_key)
     print("File uploaded to S3")
     
