eb22b420b6a1e5195d2966d69e16984a20743e20
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-03-26 21:29:42 +0900

    Update and rename S04_Mosaic_Clipped_Raster_YELL.py to S04_Mosaic_Clipped_Raster_PUUM.py

--- 02_scripts/S04_Mosaic_Clipped_Raster_PUUM.py
+++ 02_scripts/S04_Mosaic_Clipped_Raster_PUUM.py
@@ -30,26 +30,19 @@ gdal.SetConfigOption('CHECK_DISK_FREE_SPACE', 'FALSE')
 src_files_to_mosaic = []
 
 file_ID = [
-           #'015',
-           '021',
-           '022',
-           '024',
-           '013',
            '005',
-           '026',
-           '014',
-           '012',
-           '019',
-           '003',
-           '046',
-           '011'
+           '020',
+           '017',
+           '004',
+           '013',
+           '032'
            ]
 
 for i,ID in enumerate(file_ID):
     src_files_to_mosaic = []
     # List files associated with a single buffer shape
     search_criteria = str(ID)
-    dirpath = "YELL_flightlines/Site_boundaries/YELL/"
+    dirpath = "PUUM_flightlines/Site_boundaries/PUUM/"
 
     # List objects in the S3 bucket in the matching directory
     objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
@@ -107,17 +100,17 @@ for i,ID in enumerate(file_ID):
         "height": mosaic.shape[1],
         "width": mosaic.shape[2],
         "transform": out_trans,
-        "crs": "+init=epsg:32612 +units=m +no_defs "}) # for TALL UTM WGS 16N
+        "crs": "+init=epsg:32605 +units=m +no_defs "}) # for TALL UTM WGS 16N
     print(out_meta)
 
     # Write to computer, send to S3
-    local_file_path = Out_Dir + "/mosaic_YELL.tif"
+    local_file_path = Out_Dir + "/mosaic_PUUM.tif"
     with rasterio.open(local_file_path, "w", **out_meta) as dest:
         dest.write(mosaic)
     print("File written")
     
     # Push to S3 bucket
-    destination_s3_key = 'YELL_flightlines/Mosaic_YELL_'+str(ID)+'.tif'
+    destination_s3_key = 'PUUM_flightlines/Mosaic_PUUM_'+str(ID)+'.tif'
     upload_to_s3(bucket_name, local_file_path, destination_s3_key)
     print("File uploaded to S3")
     
