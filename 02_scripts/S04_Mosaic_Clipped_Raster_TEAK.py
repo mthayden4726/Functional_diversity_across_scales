92b715d6284c589bce404a21d15b950786190b1c
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-04-05 10:14:52 -0600

    Lst two

--- 02_scripts/S04_Mosaic_Clipped_Raster_TEAK.py
+++ 02_scripts/S04_Mosaic_Clipped_Raster_TEAK.py
@@ -30,16 +30,16 @@ gdal.SetConfigOption('CHECK_DISK_FREE_SPACE', 'FALSE')
 src_files_to_mosaic = []
 
 file_ID = [
-  'site_0',
-  'site_1',
-  'site_10',
-  'site_11',
-  'site_12',
-  'site_2',
-  'site_3',
-  'site_4',
-          'site_5',
-          'site_6',
+  #'site_0',
+  #'site_1',
+  #'site_10',
+  #'site_11',
+  #'site_12',
+  #'site_2',
+  #'site_3',
+  #'site_4',
+          #'site_5',
+          #'site_6',
           'site_7',
           'site_9']
 
