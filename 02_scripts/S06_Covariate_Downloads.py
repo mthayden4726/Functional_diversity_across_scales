805695a348dbf54605758fa385384bf8d90ee4fe
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-04-16 11:02:35 -0600

    Do for CHM and Slope

--- 02_scripts/S06_Covariate_Downloads.py
+++ 02_scripts/S06_Covariate_Downloads.py
@@ -79,19 +79,19 @@ for i,file in enumerate(file_paths):
 file_names = list(file_names)  # Convert set back to a list if needed
 print(file_names)
 
-for i, file in enumerate(file_names):
-  print(file)
-  flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/D15/2019_ONAQ_2/L3/DiscreteLidar/DTMGtif/NEON_D15_ONAQ_DP3_' + file +'_DTM.tif'
-  files = []
-  files.append(flight)
-  retrieve_neon_files(files, Data_Dir)
-  local_file_path = Data_Dir + "/NEON_D15_ONAQ_DP3_" + file + '_DTM.tif'
-  destination_s3_key = 'Environmental_Covariates/ONAQ/DTM_' + str(file) + '.tif'
+#for i, file in enumerate(file_names):
+  #print(file)
+  #flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/D15/2019_ONAQ_2/L3/DiscreteLidar/DTMGtif/NEON_D15_ONAQ_DP3_' + file +'_DTM.tif'
+  #files = []
+  #files.append(flight)
+  #retrieve_neon_files(files, Data_Dir)
+  #local_file_path = Data_Dir + "/NEON_D15_ONAQ_DP3_" + file + '_DTM.tif'
+  #destination_s3_key = 'Environmental_Covariates/ONAQ/DTM_' + str(file) + '.tif'
   #dtm_tif = rasterio.open(img)
   
-  upload_to_s3(bucket_name, local_file_path, destination_s3_key)
-  os.remove(local_file_path)
-  print("flightline complete")
+  #upload_to_s3(bucket_name, local_file_path, destination_s3_key)
+  #os.remove(local_file_path)
+  #print("flightline complete")
 
 # Canopy Height (DP3.30015.001)
 
@@ -139,3 +139,46 @@ for i, file in enumerate(file_names):
   print("flightline complete")
 
 # Slope/Aspect (DP3.30025.001) 
+
+PRODUCTCODE = 'DP3.30025.001'
+
+# Build so that we can loop through reading files in
+url = SERVER+'data/'+PRODUCTCODE+'/'+SITECODE+'/'+YEAR
+# Request the url
+data_request = requests.get(url)
+# Convert the request to Python JSON object
+data_json = data_request.json()
+print(data_json)
+# Create list of file paths of interest for given site, product, year
+file_paths = []
+for file in data_json['data']['files'][:]:
+  if 'slope.tif' in file['name']:
+    file_paths.insert(1, file['url'])
+    #print(file['url'])
+print(file_paths)
+
+file_names = set()
+for i,file in enumerate(file_paths):
+    match = re.search(r'DP3_(.*?)_slope', file)
+    if match:
+        file_name = match.group(1)
+        print(file_name)
+        file_names.add(file_name)
+    else:
+        print("Pattern not found in the URL.")
+file_names = list(file_names)  # Convert set back to a list if needed
+print(file_names)
+
+for i, file in enumerate(file_names):
+  print(file)
+  flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/D15/2019_ONAQ_2/L3/DiscreteLidar/SlopeGtif/NEON_D15_ONAQ_DP3_' + file +'_slope.tif'
+  files = []
+  files.append(flight)
+  retrieve_neon_files(files, Data_Dir)
+  local_file_path = Data_Dir + "/NEON_D15_ONAQ_DP3_" + file + '_CHM.tif'
+  destination_s3_key = 'Environmental_Covariates/ONAQ/Slope_' + str(file) + '.tif'
+  #dtm_tif = rasterio.open(img)
+  
+  upload_to_s3(bucket_name, local_file_path, destination_s3_key)
+  os.remove(local_file_path)
+  print("flightline complete")
