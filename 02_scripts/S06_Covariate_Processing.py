6291d9f11bc0045fd934d9016ee4837a83006567
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-04-16 10:25:08 -0600

    Update S06_Covariate_Processing.py

--- 02_scripts/S06_Covariate_Processing.py
+++ 02_scripts/S06_Covariate_Processing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue April 16 2024
+Created on Tue Aug 29 10:28:57 2023
 
 @author: meha3816
 """
@@ -26,7 +26,7 @@ from tqdm.contrib.concurrent import process_map
 from multiprocessing import Pool, cpu_count
 from S01_Moving_Window_FRIC import * # add scripts folder to python path manager
 from S01_Functions_KONZ import * # add scripts folder to python path manager
-#from osgeo import gdal, osr
+from osgeo import gdal, osr
 from matplotlib.pyplot import subplots, show
 import h5py
 import sys
@@ -41,27 +41,19 @@ Out_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
 bucket_name = 'bioscape.gra'
 s3 = boto3.client('s3')
 
-# Select site
-site = "ONAQ"
+nir_band = 90
+red_band = 58
+ndvi_threshold = 0.25
+epsg = 32619
 
-# List objects in the S3 bucket in the matching directory
-dirpath = "NEON BRDF-TOPO Corrections/2019_" + site + "/"
-objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
-# Filter objects based on the search criteria
-files = [obj['Key'] for obj in objects if obj['Key'].endswith('.json') and (search_criteria in obj['Key'])]
-file_names = set()
-for i,file in enumerate(files):
-    match = re.search(r'DP1_(.*?)_reflectance', file)
-    if match:
-        file_name = match.group(1)
-        print(file_name)
-        file_names.add(file_name)
-    else:
-        print("Pattern not found in the URL.")
-file_names = list(file_names)  # Convert set back to a list if needed
-print(file_names)
+## Find files for:
+# Elevation (DP3.30024.001)
 
-# Loop through all site files
+# Canopy Height (DP3.30015.001)
+
+# Slope/Aspect (DP3.30025.001) 
+
+# Loop through all CLBJ files
 for i,file in enumerate(file_names):
 
     # Set to none to reduce memory use
@@ -80,34 +72,18 @@ for i,file in enumerate(file_names):
     mask = None
     
     print(file)
-    flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/' + domain + '/' + site_id + '/L1/Spectrometer/ReflectanceH5/' + date_id + '/NEON_' + domain + '_' + site + '_DP1_' + file +'_reflectance.h5'
+    flight = 'https://storage.googleapis.com/neon-aop-products/2019/FullSite/D01/2019_HARV_6/L1/Spectrometer/ReflectanceH5/2019082013/NEON_D01_HARV_DP1_' + file +'_reflectance.h5'
     files = []
     files.append(flight)
-    print(files)
     try:
         retrieve_neon_files(files, Data_Dir)
     except Exception as e:
         continue 
-    img = Data_Dir + "/NEON_" + domain + "_" + site + "_DP1_" + file + '_reflectance.h5'
+    img = Data_Dir + "/NEON_D01_HARV_DP1_" + file + '_reflectance.h5'
     neon = ht.HyTools() 
     neon.read_file(img,'neon')
-    print("file loaded")
-    topo_file = "NEON BRDF-TOPO Corrections/2019_" + site + "/NEON_" + domain + "_" + site + "_DP1_" + file + "_reflectance_topo_coeffs_topo.json"
-    brdf_file = "NEON BRDF-TOPO Corrections/2019_" + site + "/NEON_" + domain + "_" + site + "_DP1_" + file + "_reflectance_brdf_coeffs_topo_brdf.json"
-    try:
-      s3.download_file(bucket_name, topo_file, Data_Dir + '/topo.json')
-      s3.download_file(bucket_name, brdf_file, Data_Dir + '/brdf.json')
-    except Exception as e:
-        continue
-    print("Files downloaded successfully.")
-    topo_coeffs = Data_Dir + "/topo.json"
-    brdf_coeffs = Data_Dir + "/brdf.json"
-    neon.load_coeffs(topo_coeffs,'topo')
-    neon.load_coeffs(brdf_coeffs, 'brdf')
-    print("corrections loaded")
     # Store map info for raster
     refl_md, header_dict = store_metadata(neon, epsg)
-    print("EPSG:", refl_md['epsg'])
     # Export with corrections
     wavelength = header_dict['wavelength']
     good_wl = np.where((wavelength < 1340) | (wavelength > 1955), wavelength, np.nan)
@@ -126,7 +102,7 @@ for i,file in enumerate(file_names):
     print("Shape of mask array:", mask.shape)
     print("masking by ndvi")
     fullarraystack[mask, :] = np.nan
-    destination_s3_key = site + '_flightlines/'+ str(file)+'_output_' + '.tif'
+    destination_s3_key = 'HARV_flightlines/'+ str(file)+'_output_' + '.tif'
     local_file_path = Out_Dir + '/output_fullarray_' + file + '.tif'
     print(local_file_path)
     print("rasterizing array")
