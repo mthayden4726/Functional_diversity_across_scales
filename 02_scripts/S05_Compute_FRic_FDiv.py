bf0b75748b7a69961f97558eb1fdda7e2ef52ee2
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-05-10 10:04:44 -0600

    Update S05_Compute_FRic_FDiv.py

--- 02_scripts/S05_Compute_FRic_FDiv.py
+++ 02_scripts/S05_Compute_FRic_FDiv.py
@@ -53,19 +53,18 @@ comps = 3 # default component numbers for PCA
 
 # Loop through clipped files
 # Choose site and plots
-file_stem = 'ONAQ_flightlines/Mosaic_ONAQ_'
+file_stem = 'TALL_flightlines/Mosaic_TALL_'
 plots = [
-              '005',
-  '008',
-  '010',
-  '011',
-  '018',
-  '019',
-  '021',
-  '024',
-  '030',
-  '043',
-  '073'
+              '001',
+  '002',
+  '009',
+  '013',
+  '012',
+  '020',
+  '026',
+  '027',
+  '032',
+  '044'
 ]
 
 # Loop through plots
@@ -112,14 +111,14 @@ for i in plots:
     print(pca_x.shape)
     # Calculate FRic on PCA across window sizes
     results_FR = {}
-    local_file_path_fric = Out_Dir + "/ONAQ_fric_" + str(i) + "_v2.csv"
+    local_file_path_fric = Out_Dir + "/TALL_fric_" + str(i) + "_v3.csv"
     window_batches = [(a, pca_x, results_FR, local_file_path_fric) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs,
         window_batches,
         max_workers=cpu_count() - 1
     )
-    destination_s3_key_fric = "/ONAQ_fric_veg_" + str(i) + "_v2.csv"
+    destination_s3_key_fric = "/TALL_fric_veg_" + str(i) + "_v3.csv"
     #f = open(local_file_path,"w")
     # write file
     #f.write(str(volumes))
@@ -129,7 +128,7 @@ for i in plots:
     print("FRic file uploaded to S3")
     # Calculate FDiv on PCA across window sizes
     results_FD = {}
-    local_file_path_fdiv = Out_Dir + "/ONAQ_fdiv_veg_" + str(i) + "_v2.csv"
+    local_file_path_fdiv = Out_Dir + "/TALL_fdiv_veg_" + str(i) + "_v3.csv"
     window_batches = [(a, pca_x, results_FD, local_file_path_fdiv) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
     volumes = process_map(
         window_calcs_fdiv,
@@ -137,7 +136,7 @@ for i in plots:
         max_workers=cpu_count() - 1
     )
     # open file for writing
-    destination_s3_key_fdiv = "/ONAQ_fdiv_veg_" + str(i) + "_v2.csv"
+    destination_s3_key_fdiv = "/TALL_fdiv_veg_" + str(i) + "_v3.csv"
     upload_to_s3(bucket_name, local_file_path_fdiv, destination_s3_key_fdiv)
     print("FDiv file uploaded to S3")
     os.remove(file)
