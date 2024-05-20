34481549defcbde6bfa1fc462d8b09302404495a
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-05-20 08:42:31 -0600

    Add comments

--- 02_scripts/S05_Compute_FRic_FDiv.py
+++ 02_scripts/S05_Compute_FRic_FDiv.py
@@ -1,4 +1,17 @@
-## Import packages ##
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script to compute functional richness and divergence for a set of moving windows across a raster.
+
+Author: M. Hayden
+Updated: May 20, 2024
+
+User input:
+1. Name of the NEON site (e.g., BART)
+    
+"""
+
+# Load required libraries
 import hytools as ht
 import matplotlib.pyplot as plt
 import matplotlib.colors as clr
@@ -35,11 +48,11 @@ from fiona.crs import from_epsg
 import pycrs
 import csv
 from csv import writer
-# Import functions defined in S01_specdiv_functions.py
-from S01_Functions import * # add scripts folder to python path manager
+import argparse
+# Import supporting functions, functions for calculating FRic and FDiv
+from S01_Functions import *
 from S01_Moving_Window_FRIC import *
 from S01_Moving_Window_FDiv import *
-import argparse
 
 # Set directories
 Data_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
@@ -47,10 +60,10 @@ Out_Dir = '/home/ec2-user/BioSCape_across_scales/03_output'
 bucket_name = 'bioscape.gra'
 s3 = boto3.client('s3')
 
-## Set global parameters ##
-# window_sizes = [10, 30, 60, 120]   # list of window sizes to test
-window_sizes = [60, 120, 240, 480, 960, 1200, 1500, 2000, 2200]
-comps = 3 # default component numbers for PCA
+# Set global parameters #
+# window_sizes = [10, 30, 60, 120]   # smaller list of window sizes to test
+window_sizes = [60, 120, 240, 480, 960, 1200, 1500, 2000, 2200] # full list of window size for computations
+comps = 3 # number of components for PCA
 
 # Use arg parse for local variables
 # Create the parser
@@ -64,8 +77,6 @@ args = parser.parse_args()
 
 # Assign the arguments to variables
 SITECODE = args.SITECODE
-
-# Loop through clipped files
 
 # Choose site and plots
 file_stem = SITECODE + '_flightlines/Mosaic_' + SITECODE + '_'
@@ -89,26 +100,31 @@ for i,shp in enumerate(shapefiles):
 plots = list(shapefile_names)  # Convert set back to a list if needed
 print(plots)
 
-# Loop through plots
+# Loop through plots to calculate FRic and FDiv
 for i in plots:
-    clip_file = file_stem + str(i) + '.tif'
+    # Load data
+    clip_file = file_stem + str(i) + '.tif' # Define file name in S3
     print(clip_file)
     # Download plot mosaic
     s3.download_file(bucket_name, clip_file, Data_Dir + '/mosaic.tif')
-    file = Data_Dir + '/mosaic.tif'
+    file = Data_Dir + '/mosaic.tif' # Define local file name
+    print("Raster loaded")
     # Open as raster
     raster = rxr.open_rasterio(file, masked=True)
     print(raster)
     # Convert data array to numpy array
     veg_np = raster.to_numpy()
     shape = veg_np.shape
-    print(shape)
+    print("Raster shape:", shape)
+    
+    # Process for PCA
     # Flatten features into one dimesnion
     dim1 = shape[1]
     dim2 = shape[2]
     bands = shape[0]
     X = veg_np.reshape(bands,dim1*dim2).T
     print(X.shape)
+    # Set no data to nan
     X = X.astype('float32')
     X[np.isnan(X)] = np.nan
     X[X < 0] = np.nan
@@ -116,22 +132,22 @@ for i in plots:
     # Impute values for NAs
     imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
     X_transformed = imputer.fit_transform(X)
-    # Change nan to 0 
-    #X_no_nan = np.nan_to_num(X, nan=0)
-    # Take mean 
+    # Scale & standardize array 
     x_mean = X_transformed.mean(axis=0)[np.newaxis, :]
-    # Scale & standardize array
     X_transformed -=x_mean
     x_std = np.nanstd(X_transformed,axis=0)[np.newaxis, :]
     X_transformed /=x_std
     # Perform initial PCA fit
+    print("Fitting PCA")
     pca = PCA(n_components=comps) # set max number of components
     pca.fit(X_transformed)
+    # PCA transform
     pca_x =  pca.transform(X_transformed)
-    print(pca_x)
     pca_x = pca_x.reshape((dim1, dim2,comps))
-    print(pca_x.shape)
+    print("PCA shape:", pca_x.shape)
+    
     # Calculate FRic on PCA across window sizes
+    print("Calculating FRic")
     results_FR = {}
     local_file_path_fric = Out_Dir + "/" + SITECODE + "_fric_" + str(i) + ".csv"
     window_batches = [(a, pca_x, results_FR, local_file_path_fric) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
@@ -141,14 +157,11 @@ for i in plots:
         max_workers=cpu_count() - 1
     )
     destination_s3_key_fric = "/" + SITECODE + "_fric_veg_" + str(i) + ".csv"
-    #f = open(local_file_path,"w")
-    # write file
-    #f.write(str(volumes))
-    # close file
-    #f.close()
     upload_to_s3(bucket_name, local_file_path_fric, destination_s3_key_fric)
     print("FRic file uploaded to S3")
+    
     # Calculate FDiv on PCA across window sizes
+    print("Calculating FDiv")
     results_FD = {}
     local_file_path_fdiv = Out_Dir + "/" + SITECODE + "_fdiv_veg_" + str(i) + ".csv"
     window_batches = [(a, pca_x, results_FD, local_file_path_fdiv) for a in np.array_split(window_sizes, cpu_count() - 1) if a.any()]
@@ -161,9 +174,12 @@ for i in plots:
     destination_s3_key_fdiv = "/" + SITECODE + "_fdiv_veg_" + str(i) + ".csv"
     upload_to_s3(bucket_name, local_file_path_fdiv, destination_s3_key_fdiv)
     print("FDiv file uploaded to S3")
+
+    # Remove files to clear storage
     os.remove(file)
     X = None
     X_no_nan = None
     pca_x = None
     veg_np = None
+    
     print("Mosaic Complete - Next...")
