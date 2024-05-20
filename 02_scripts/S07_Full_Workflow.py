a14ef59450971cdac741e59d0a2913ae61af6d08
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-05-20 09:13:37 -0600

    Update S07_Full_Workflow.py

--- 02_scripts/S07_Full_Workflow.py
+++ 02_scripts/S07_Full_Workflow.py
@@ -1,61 +1,56 @@
-## CURRENTLY IN PROGRESS - NOT RUNNING ###
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script that loops through individual files to correct, clip, mosaic and compute FRic & FDiv for plots within NEON site.
 
-## Workflow for going from original NEON flightline to output of functional metrics ##
-# Pulls functions from multiple other scripts.
+User must define parameters that feed into calling each script. 
 
-# Last updated on January 23rd, 2024
 
-# Import functions
-from S01_Functions import * 
-from S02_Topo_BRDF_Corrections_All import *
-import csv
+Author: M. Hayden
+Last Updated: 1/23/24
+"""
 
-# Set working directories
-Data_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/01_rawdata'
-Out_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
-bucket_name = 'bioscape.gra'
-s3 = boto3.client('s3')
+# Load required libraries
+import os
 
-# Set global parameters
-window_sizes = [60, 120, 240, 480, 700, 960, 1200, 1500, 2000, 2200]
-ndvi_threshold = 0.4 # ndvi threshold for radiometric filtering
-comps = 3 # default component numbers for PCA
+# Define variables
+SITECODE = 'BART'
+DOMAIN_ID = 'D01'
+ID_NO = '5'
+YEAR = '201908'
+DATE = '20190825'
+DATE_ID = '2019082513'
+EPSG = '36219'
+NDVI_THRESHOLD = 0.25
 
-# Define NEON sites of interest
-NEON_sites = ['TEAK', 'SERC', 'SRER']
 
-# Pull list of flightlines for each site from S3
-flightlines_file = "NEON_flightlines.csv"
-s3.download_file(bucket_name, flightlines_file, Data_Dir + '/NEON_flightlines.csv')
-print("File downloaded successfully.")
-
-# Load list into array and filter for sites of interest
-with open(Data_Dir + '/NEON_flightlines.csv', newline='') as csvfile:
-    all_flights = list(csv.reader(csvfile))
-my_flights = all_flights[all_flights["Site"].isin(NEON_sites)]
-
-## PROCESSING WORKFLOW ##
+# Workflow to get Functional Richness and Functional Divergence for set of NEON plots (within a site)
 
 # 1. Topo/BRDF corrections
-topo_brdf_correct(my_flights)
-print("Corrections complete")
+script_string = "python 02_scripts/S02_TopoBRDF_Corrections.py --SITECODE " + SITECODE 
+                + "--DOMAIN " + DOMAIN_ID 
+                + "--ID_NO " + ID_NO  
+                + "--DATE " + DATE 
+                + "--DATE_ID " + DATE_ID 
+                + "--EPSG " + EPSG 
+                + "--NDVI " + NDVI_THRESHOLD
+os.system(script_string)
+print("Corrections complete.")
 
 # 2. Clip to regions of interest
-clip_flightline(corrected_flightlines)
-print("Flightlines Clipped")
+script_string = "python 02_scripts/S03_Clip_Corrected.py --SITECODE " + SITECODE 
+                + "--YEAR " + YEAR 
+os.system(script_string)
+print("Flightlines clipped.")
 
 # 3. Mosaic flightlines
-mosaic_flightlines(clipped_flightlines)
-print("Flightlines mosaicked")
+script_string = "python 02_scripts/S04_Mosaic_Clipped_Raster.py --SITECODE " + SITECODE 
+                + "--EPSG " + EPSG
+os.system(script_string)
+print("Flightlines mosaicked.")
 
 # 4. Compute Functional Richness
-compute_fric(all_mosaics)
-print("FRic computed for all mosaics")
+script_string = "python 02_scripts/S05_Compute_FRic_FDiv.py --SITECODE " + SITECODE 
+os.system(script_string)
+print("FRic & FDiv computed for all mosaics.")
 
-# 5. Compute Functional Evenness
-compute_feve(all_mosaics)
-print("FEve computed for all mosaics")
-
-# 6. Compute Functional Divergence
-compute_fdiv(all_mosaics)
-print("FDiv computed for all mosaics")
