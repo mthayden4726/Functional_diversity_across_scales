1e058c85e2851acef9774b305ffe69df4ff46f24
Meghan Hayden <70178120+mthayden4726@users.noreply.github.com>
2024-05-20 08:30:59 -0600

    Add comments

--- 02_scripts/S03_Clip_Corrected.py
+++ 02_scripts/S03_Clip_Corrected.py
@@ -1,8 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script to pull corrected rasters from S3, clip them to the extent for the field sites, and then re-export them to S3.
 
-# Script to pull rasters from S3, clip them to the extent for the field sites, and then re-export them to S3.
-# Next they will be pulled in to be merged. 
-# Meghan Hayden - 2/22/2024
+Author: M. Hayden
+Updated: May 20, 2024
 
+User input:
+1. Name of the NEON site (e.g., BART)
+2. Year, Month of desired files (e.g., 201908). Some sites had flights occur at multiple times throughout the year - 
+    for mosaicking purposes, we want to combine flights that were part of the same survey (or occurred at around the same time).
+    
+"""
+
+# Load required libraries
 import rasterio
 from rasterio.merge import merge
 from rasterio.plot import show
@@ -20,6 +31,7 @@ from osgeo import gdal
 from S01_Functions import *
 import argparse
 
+# Set global parameters
 Data_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/01_rawdata'
 Out_Dir = '/home/ec2-user/BioSCape_across_scales/01_data/02_processed'
 bucket_name = 'bioscape.gra'
@@ -40,10 +52,10 @@ args = parser.parse_args()
 SITECODE = args.SITECODE
 YEAR = args.YEAR
 
-# Find files for mosaicing (define search terms)
+# Load corrected rasters for clipping
+# Define search terms
 search_criteria = YEAR
 dirpath = SITECODE + "_flightlines/"
-
 # List objects in the S3 bucket in the matching directory
 objects = s3.list_objects_v2(Bucket=bucket_name, Prefix=dirpath)['Contents']
 # Filter objects based on the search criteria
@@ -68,9 +80,10 @@ for i,shp in enumerate(shapefiles):
 shapefile_names = list(shapefile_names)  # Convert set back to a list if needed
 print(shapefile_names)
 
-# Load the polygon for clipping ()
+# Clip all files that overlap with each shapefile
 for j,shp in enumerate(shapefile_names):
-    print(shp)
+    # Load shapefile
+    print('Loading shapefile:', shp)
     shape = 'Site_boundaries/' + SITECODE + '/' + SITECODE + '_' + shp
     downloaded_files = download_shapefile(bucket_name, shape, Out_Dir)
     shapefile_path = next(file for file in downloaded_files if file.endswith('.shp'))
@@ -79,14 +92,10 @@ for j,shp in enumerate(shapefile_names):
     with fiona.open(shapefile_path, "r") as shapefile:
         shapes = [feature["geometry"] for feature in shapefile]
     print(shapes)
-    #minx, miny, maxx, maxy = box(*shapes[0].bounds).bounds
-    
-    # Access the bounds of the entire GeoDataFrame
-    #gdf = gpd.read_file(shapefile_path)
-    #minx, miny, maxx, maxy = gdf.total_bounds
-    
+
+    # Loop through corrected rasters and clip those with overlap
     for i, file in enumerate(files):
-        print('Loading file from S3')
+        print('Loading raster:', i)
         s3.download_file(bucket_name, file, Out_Dir + '/file_' + str(i) + '.tif')
         flight = Out_Dir + '/file_' + str(i) + '.tif'
         print(flight)
@@ -94,7 +103,7 @@ for j,shp in enumerate(shapefile_names):
             try:
                 out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True) # remove nodata = 0 to see if this helps with mosaic
                 out_meta = src.meta
-                print("File Clipped")
+                print("File clipped")
                 print(out_meta)
                 out_meta.update({"driver": "GTiff",
                     "height": out_image.shape[1],
@@ -103,7 +112,7 @@ for j,shp in enumerate(shapefile_names):
                 local_file_path = Out_Dir + "/clip.tif"
                 with rasterio.open(local_file_path, "w", **out_meta) as dest:
                     dest.write(out_image)
-                    print("File Written")
+                    print("File written")
                 destination_s3_key = SITECODE + '_flightlines/' + str(shape) + '_Clipped_file_' + str(i) + '.tif'
                 upload_to_s3(bucket_name, local_file_path, destination_s3_key)
                 print("File uploaded to S3")
